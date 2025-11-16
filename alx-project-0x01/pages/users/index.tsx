f4c2c278406be3a2621360558993f9f62946793d
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-16 17:48:27 +0100

    refactor interfaces Userprops configure types

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,13 @@
+import UserCard from '@/components/common/UserCard';
+import { UserProps } from '@/interfaces';
+
+interface PageProps {
+  posts: UserProps[];
+}
+
 export async function getStaticProps() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const posts = await response.json();
+  const posts: UserProps[] = await response.json();
 
   return {
     props: {
@@ -8,3 +15,17 @@ export async function getStaticProps() {
     },
   };
 }
+
+const UsersPage: React.FC<PageProps> = ({ posts }) => {
+  return (
+    <div>
+      <h1>Users</h1>
+
+      {posts.map((user) => (
+        <UserCard key={user.id} {...user} />
+      ))}
+    </div>
+  );
+};
+
+export default UsersPage;
